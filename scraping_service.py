d63ef4b35d92f6411c5feab6c8bbfcd3f5258081
kubalomzik <57918912+kubalomzik@users.noreply.github.com>
2021-08-26 13:34:24 +0200

    Update scraping_service.py

--- scraping_service.py
+++ scraping_service.py
@@ -1,12 +1,18 @@
 from item_details import store_details
 from items import get_category_names, get_subcategory_names, get_product_urls
 
-main_url = "https://webscraper.io"
-categories = get_category_names(main_url)
 
-for category in categories:
-    subcategories = get_subcategory_names(main_url, category)
-    for subcategory in subcategories:
-        get_product_urls(main_url, category, subcategory)
+def main():
+    main_url = "https://webscraper.io"
+    categories = get_category_names(main_url)
 
-store_details()
+    for category in categories:
+        subcategories = get_subcategory_names(main_url, category)
+        for subcategory in subcategories:
+            get_product_urls(main_url, category, subcategory)
+
+    store_details()
+
+
+if __name__ == '__main__':
+    main()
